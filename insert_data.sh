887c0e20ecf741d824b5796b494637364407c19c
thebinij <binijshrestha@gmail.com>
2022-10-23 19:46:34 +0000

    Save progress: 1.1

--- insert_data.sh
+++ insert_data.sh
@@ -8,3 +8,10 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
+while IFS="," read -r year round winner opponent winner_goals opponent_goals
+do
+  echo "$($PSQL "INSERT INTO teams (name) VALUES ('$opponent'),('$winner') ON CONFLICT DO NOTHING")"
+  OPPONENT_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$opponent'")"
+  WINNER_ID="$($PSQL "SELECT team_id FROM teams WHERE name = '$winner'")"
+  echo "$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($year,'$round',$WINNER_ID,$OPPONENT_ID,$winner_goals,$opponent_goals)")"
+done < <(tail -n +2 games.csv)
